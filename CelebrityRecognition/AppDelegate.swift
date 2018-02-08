551e5f43e2383c95f8abfb5ccde5238fb01d0dab
Shaurya Sinha <ShauryaSinha@ASHUTOSHs-MacBook-Pro.local>
2018-02-08 12:49:27 -0500

    Added AWS pods

--- CelebrityRecognition/AppDelegate.swift
+++ CelebrityRecognition/AppDelegate.swift
@@ -8,16 +8,20 @@
 
 import UIKit
 import CoreData
+import AWSMobileClient
+import AWSCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        return true
+        AWSDDLog.add(AWSDDTTYLogger.sharedInstance)
+        AWSDDLog.sharedInstance.logLevel = .info
+        
+        return AWSMobileClient.sharedInstance().interceptApplication(application, didFinishLaunchingWithOptions: launchOptions)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
---
+++ Pods/Target Support Files/Pods-CelebrityRecognition/Pods-CelebrityRecognition-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CelebrityRecognition : NSObject
+@end
+@implementation PodsDummy_Pods_CelebrityRecognition
+@end
