649090ea008253de79b7f33ab1bda90f419d6d58
Shaurya Sinha <ShauryaSinha@ASHUTOSHs-MacBook-Pro.local>
2018-02-26 22:13:53 -0500

    Added App Icon and Launch Screen

---
+++ CelebrityRecognition/Models/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  CelebrityRecognition
+//
+//  Created by Shaurya Sinha on 26/02/18.
+//  Copyright © 2018 CS490Team. All rights reserved.
+//
+
+import Foundation
---
+++ CelebrityRecognition/Views/PosterCell.swift
@@ -0,0 +1,13 @@
+//
+//  PosterCell.swift
+//  CelebrityRecognition
+//
+//  Created by Shaurya Sinha on 26/02/18.
+//  Copyright © 2018 CS490Team. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+}
